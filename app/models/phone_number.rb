e97feb76650364ff80cb8780cbe5d7ad099e04e7
Cory Rolstad <llN3M3515ll@gmail.com>
2021-09-05 21:31:31 -0400

    Added additional PhoneNumber unit tests

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -3,5 +3,5 @@ class PhoneNumber < ApplicationRecord
   accepts_nested_attributes_for :contact, reject_if: lambda {|attributes| attributes['kind'].blank?}, allow_destroy: true
 
   validates :number, format: { with: /(\d{3}-){1,2}(\d{4})/, message: 'Phone number must be in 7 digit(###-####) or 10 digit (###-###-####) format.' }
-  validates :description, format: { with: /(Mobile)|(Home)|(Work)|(Fax)/, message: 'Phone Type must be Mobile, Home, Work, or Fax.' }
+  validates :description, format: { with: /(^Mobile$)|(^Home$)|(^Work$)|(^Fax$)/, message: 'Phone Type must be Mobile, Home, Work, or Fax.' }
 end
--- test/models/phone_number_test.rb
+++ test/models/phone_number_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class PhoneNumberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @sut = PhoneNumber.first    
+    @contact = Contact.new  name: "Andrew Jackson",
+                            email: "jack@jackson.com",
+                            birthday: Time.now,
+                            note: "Great!"
+    @contact.save
+  end
+
+  test "it should have valid format" do
+    sut = PhoneNumber.new contact_id: @contact.id,
+                              number: @sut.number,
+                              primary: @sut.primary,
+                              description: @sut.description
+    assert sut.valid?    
+  end
+
+  test "it should have valid format, and has to have a valid contact" do
+    sut = PhoneNumber.new contact_id: 900,
+                              number: @sut.number,
+                              primary: @sut.primary,
+                              description: @sut.description
+    assert !sut.valid?    
+  end
+
+  test "it should have valid format, and has to have a valid telephone number" do
+    sut = PhoneNumber.new contact_id: @contact.id,
+                              number: "100",
+                              primary: @sut.primary,
+                              description: @sut.description
+    assert !sut.valid?    
+  end
+
+  test "it should have valid format, and has to have a non null telephone number" do
+    sut = PhoneNumber.new contact_id: @contact.id,
+                              number: nil,
+                              primary: @sut.primary,
+                              description: @sut.description
+    assert !sut.valid?    
+  end
+
+  test "it should have valid format, and has to a description of Home|Mobile|Work|Fax" do
+    sut = PhoneNumber.new contact_id: @contact.id,
+                              number: @sut.number,
+                              primary: @sut.primary,
+                              description: "Work"
+    assert sut.valid? 
+    sut.description="Mobile"
+    assert sut.valid?
+    sut.description="Home"
+    assert sut.valid?
+    sut.description="Fax"
+    assert sut.valid?
+
+    sut.description="Not Working"
+    assert !sut.valid?
+  end
+
 end
